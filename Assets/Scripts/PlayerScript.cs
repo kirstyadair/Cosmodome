698e8a77b8491dbcfcbef8a7863a366119b23722
Kirsty Adair <kirstyaadair@gmail.com>
2019-10-22 19:55:12 +0100

    some placeholders and shooting event

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerTypes
+{
+    COMEDIAN, DAREDEVIL, LIGHTWEIGHT, HEAVYWEIGHT, DAVE
+}
+
+public class PlayerScript : MonoBehaviour
+{
+    public delegate void PlayerShot(PlayerTypes playerShooting, PlayerTypes playerHit);
+    public static event PlayerShot OnPlayerShot;
+    public PlayerTypes playerType;
+    public int approval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "GreenBullet" && playerType == PlayerTypes.LIGHTWEIGHT)
+        {
+            OnPlayerShot.Invoke(PlayerTypes.DAVE, playerType);
+        }
+        else if (other.tag == "RedBullet" && playerType == PlayerTypes.DAVE)
+        {
+            OnPlayerShot.Invoke(PlayerTypes.LIGHTWEIGHT, playerType);
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    /// <summary>
+    /// Singleton instance
+    /// </summary>
+    private static ScoreManager _instance;
+
+    public static ScoreManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject g = new GameObject("Level Manager");
+                _instance = g.AddComponent<ScoreManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        PlayerScript.OnPlayerShot += PlayerShot;
+    }
+
+    void PlayerShot(PlayerTypes shootingPlayer, PlayerTypes shotPlayer)
+    {
+        Debug.Log("Shot: " + shotPlayer + ", shooting: " + shootingPlayer);
+    }
+}
