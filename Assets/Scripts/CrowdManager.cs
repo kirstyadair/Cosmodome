45d2b26742abcc329fca931890573ed1f472cbc9
Kirsty Adair <kirstyaadair@gmail.com>
2019-11-07 17:20:32 +0000

    crowd stuff

---
+++ Assets/Scripts/CrowdManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdManager : MonoBehaviour
+{
+    int playerCount;
+    int[] playerScores;
+    int[] numOfSupporters;
+    
+    ScoreManager sm;
+    GameObject[] crowdMembers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = ScoreManager.Instance;
+        playerCount = sm.players.Count;
+        playerScores = new int[playerCount];
+        numOfSupporters = new int[playerCount];
+        crowdMembers = GameObject.FindGameObjectsWithTag("Crowd");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < playerCount; i++)
+        {
+            playerScores[i] = sm.players[i].approval.percentage;
+            numOfSupporters[i] = crowdMembers.Length * (playerScores[i] / 100);
+            Debug.Log(numOfSupporters[i]);
+        }
+
+        
+    }
+}
