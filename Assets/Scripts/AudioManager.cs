16629a72cd6163c9db47e33111e89689012afbfe
Kirsty Adair <kirstyaadair@gmail.com>
2019-12-02 19:25:20 +0000

    collision sound effects in

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip metalScrape;
+    public AudioClip crowdCheer;
+    public AudioClip eliminationBuzz;
+    public AudioClip ballAppears;
+    public AudioClip ballHits;
+    public AudioClip cannonAppears;
+    public AudioClip cannonHits;
+    public AudioClip crash;
+    public AudioClip wallCrash;
+    public AudioClip reload;
+    public AudioClip sabotage;
+    public AudioClip weaponFire1;
+    public AudioClip weaponFire2;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        PlayerScript.OnPlayerCollision += PlayerCollision;
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void PlayerCollision(GameObject playerHit)
+    {
+        audioSource.volume = 0.2f;
+        audioSource.pitch = 0.5f + Random.value;
+        audioSource.PlayOneShot(crash);
+        audioSource.PlayOneShot(metalScrape);
+    }
+}
