1fa8d67d633ccd5defe797f7eeed9134fea2962c
Jishaxe <josh@joshbe.me>
2019-12-15 22:27:41 +0000

    more thematic controls

---
+++ Assets/Scripts/ControlsUIScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsUIScript : MonoBehaviour
+{
+    Animator animator;
+
+    public float delayBeforeShowingControls;
+    public float timeBeforeHidingControls;
+
+    public void ShowControls()
+    {
+        animator.SetTrigger("Show"); 
+    }
+
+    public void HideControls()
+    {
+        animator.SetTrigger("Hide");
+    }
+
+    public IEnumerator ShowControlsAfter(float time)
+    {
+        yield return new WaitForSeconds(time);
+        ShowControls();
+        StartCoroutine(HideControlsAfter(timeBeforeHidingControls));
+    }
+
+    public IEnumerator HideControlsAfter(float time)
+    {
+        yield return new WaitForSeconds(time);
+        HideControls();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(ShowControlsAfter(delayBeforeShowingControls));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
