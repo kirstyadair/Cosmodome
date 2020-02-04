17195fe536c148b599c097d5e67894ac54c54941
Josh Lee <josh@joshbe.me>
2020-02-04 19:00:14 +0000

    character selection screen wip

---
+++ Assets/Scripts/CharacterSelection/ControllerAllocation.cs
@@ -0,0 +1,106 @@
+﻿using InControl;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class CharacterSelectionOption
+{
+    public string characterName;
+    public GameObject characterPrefab;
+    public SelectionBox chosenBy = null;
+}
+
+public class ControllerAllocation : MonoBehaviour
+{
+    public Text statusText;
+
+    public SelectionBox[] selectionBoxes;
+    public CharacterSelectionOption[] selectableCharacters;
+
+    public delegate void ControllerAllocationEvent();
+    public event ControllerAllocationEvent OnAnotherBoxChanged;
+
+    Coroutine countdownCoroutine;
+
+    [Header("How many seconds to count down before starting")]
+    public int countdownFrom = 3;
+    private void Update()
+    {
+        InputDevice controller = InputManager.ActiveDevice;
+
+        // Was X pressed?
+        if (controller.Action1.WasPressed) AllocateController(controller);
+    }
+
+    public void AllocateController(InputDevice controller)
+    {
+        SelectionBox chosenBox = null;
+        foreach (SelectionBox box in selectionBoxes)
+        {
+            if (box.controller == controller) return; // this controller is already allocated, ignore
+            if (box.controller == null)
+            {
+                chosenBox = box; // This is a box with no controller assigned, so assign it
+                break;
+            }
+        }
+
+        if (chosenBox == null) return; // Tried to connect more than 4 controllers
+
+        chosenBox.AllocateController(controller, this);
+        chosenBox.OnReadyUp += CheckIfReadyToGo;
+        chosenBox.OnUnReady += CheckIfReadyToGo;
+
+
+        CheckIfReadyToGo();
+    }
+
+    public void CheckIfReadyToGo()
+    {
+        OnAnotherBoxChanged?.Invoke(); // let the other boxes know a selection was updated
+
+        // Interrupted during countdown
+        if (countdownCoroutine != null)
+        {
+            StopCoroutine(countdownCoroutine);
+            countdownCoroutine = null;
+        }
+
+        int isReady = 0;
+        int isAssigned = 0;
+        foreach (SelectionBox box in selectionBoxes)
+        {
+            if (box.isReady) isReady++;
+            if (box.controller != null) isAssigned++;
+        }
+
+        if (isAssigned == 0) statusText.text = "PRESS X TO JOIN";
+        if (isAssigned == 1) statusText.text = "NEED AT LEAST 2 PLAYERS TO START";
+        if (isAssigned >= 2) statusText.text = "WAITING FOR EVERYONE TO READY UP";
+        if (isReady == isAssigned && isReady >= 2) StartCountdown();
+    }
+
+    public void Start()
+    {
+        CheckIfReadyToGo();
+    }
+
+    public void StartCountdown()
+    {
+        countdownCoroutine = StartCoroutine(Countdown());
+    }
+
+    IEnumerator Countdown()
+    {
+        for (int count = countdownFrom; count > 0; count--)
+        {
+            statusText.text = "STARTING IN: " + count;
+            yield return new WaitForSeconds(1f);
+        }
+
+        statusText.text = "LET'S GO!";
+    }
+}
---
+++ Assets/Scripts/CharacterSelection/SelectionBox.cs
@@ -0,0 +1,158 @@
+﻿using InControl;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class SelectionBox : MonoBehaviour
+{
+    bool readyDebounce = true;
+    public bool isReady = false;
+
+    public delegate void SelectionBoxEvent();
+    public event SelectionBoxEvent OnReadyUp;
+    public event SelectionBoxEvent OnUnReady;
+
+    public InputDevice controller = null;
+
+    public GameObject characterBox;
+    public Text characterName;
+
+    public GameObject joinText;
+    public GameObject confirmText;
+    public GameObject readyText;
+    public GameObject alreadyPicked;
+
+    public Image arrowLeft;
+    public Image arrowRight;
+    public Image xButtonImage;
+
+    public Color xButtonReadyColour;
+    public Color xButtonUnReadyColour;
+    public Color arrowHighlightColour;
+    public Color arrowNormalColour;
+    public Color characterNameNormalColour;
+    public Color characterNameUsedColour;
+
+    int currentSelectedCharacter = 0;
+    ControllerAllocation controllerAllocation;
+    CharacterSelectionOption selectedCharacter = null;
+
+    public void AllocateController(InputDevice controller, ControllerAllocation controllerAllocation)
+    {
+        this.controller = controller;
+        this.controllerAllocation = controllerAllocation;
+        this.controllerAllocation.OnAnotherBoxChanged += OnAnotherBoxChanged;
+
+        ShowCharacterSelection();
+        ChangeSelection(0);
+        readyDebounce = true; // to avoid accidentially clicking thru to ready up
+    }
+
+    public void ShowCharacterSelection()
+    {
+        arrowLeft.gameObject.SetActive(true);
+        arrowRight.gameObject.SetActive(true);
+        joinText.gameObject.SetActive(false);
+        characterBox.SetActive(true);
+        confirmText.SetActive(true);
+        readyText.SetActive(false);
+        xButtonImage.color = xButtonUnReadyColour;
+    }
+
+    public void ShowReadyUp()
+    {
+        arrowLeft.gameObject.SetActive(false);
+        arrowRight.gameObject.SetActive(false);
+        joinText.gameObject.SetActive(false);
+        confirmText.SetActive(false);
+        characterBox.SetActive(true);
+        readyText.SetActive(true);
+        xButtonImage.color = xButtonReadyColour;
+    }
+
+    public void SelectRight()
+    {
+        currentSelectedCharacter++;
+        if (currentSelectedCharacter >= controllerAllocation.selectableCharacters.Length) currentSelectedCharacter = 0;
+
+        ChangeSelection(currentSelectedCharacter);
+    }
+
+    public void OnAnotherBoxChanged()
+    {
+        ChangeSelection(currentSelectedCharacter);
+    }
+
+    public void ChangeSelection(int i)
+    {
+        selectedCharacter = controllerAllocation.selectableCharacters[i];
+        characterName.text = selectedCharacter.characterName;
+
+        if (selectedCharacter.chosenBy != null && selectedCharacter.chosenBy != this)
+        {
+            characterName.color = characterNameUsedColour;
+            alreadyPicked.SetActive(true);
+        }
+        else
+        {
+            characterName.color = characterNameNormalColour;
+            alreadyPicked.SetActive(false);
+        }
+    }
+
+    public void SelectLeft()
+    {
+        currentSelectedCharacter--;
+        if (currentSelectedCharacter < 0) currentSelectedCharacter = controllerAllocation.selectableCharacters.Length - 1;
+
+        ChangeSelection(currentSelectedCharacter);
+    }
+
+    public void Update()
+    {
+        if (controller == null) return;
+
+
+        if (!isReady)
+        {
+            // Pressed X on selection screen, ready up
+            if (controller.Action1.WasPressed && selectedCharacter.chosenBy == null)
+            {
+                if (readyDebounce)
+                {
+                    readyDebounce = false;
+                    return;
+                }
+
+                ShowReadyUp();
+                selectedCharacter.chosenBy = this;
+                isReady = true;
+                OnReadyUp?.Invoke();
+            }
+
+            // Highlight the selection arrows when left D pad or left stick is pressed
+            if (controller.LeftStickLeft.IsPressed || controller.DPadLeft.IsPressed) arrowLeft.color = arrowHighlightColour;
+            else arrowLeft.color = arrowNormalColour;
+
+            if (controller.LeftStickRight.IsPressed || controller.DPadRight.IsPressed) arrowRight.color = arrowHighlightColour;
+            else arrowRight.color = arrowNormalColour;
+
+            // once we've done the click, change selection
+            if (controller.LeftStickLeft.WasPressed || controller.DPadLeft.WasPressed) SelectLeft();
+            if (controller.LeftStickRight.WasPressed || controller.DPadRight.WasPressed) SelectRight();
+
+        } else
+        {
+            // Pressed X or O while readied up, so unready
+            if (controller.Action1.WasPressed || controller.Action2.WasPressed)
+            {
+                ShowCharacterSelection();
+                selectedCharacter.chosenBy = null;
+                isReady = false;
+                OnUnReady?.Invoke();
+            }
+        }
+    }
+}
