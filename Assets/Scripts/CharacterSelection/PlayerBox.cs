b8a29a1e09e0092bff1d3c915db2f93c84c25f86
Josh Lee <josh@joshbe.me>
2020-02-17 13:24:19 +0000

    uwu

---
+++ Assets/Scripts/CharacterSelection/PlayerBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBox : MonoBehaviour
+{
+    public int playerNumber;
+    public Color playerColour;
+
+    public void AssignController()
+    {
+
+    }
+
+    public void Selecting()
+    {
+
+    }
+}
