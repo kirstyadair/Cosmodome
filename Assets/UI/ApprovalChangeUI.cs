72c76ecb4ab0922a5f1bedfcc9ce7c8a694799ae
jokehboy <jokehboy@gmail.com>
2020-02-23 11:18:50 +0000

    WIP UI animations

--- Assets/UI/ApprovalChangeUI.cs
+++ Assets/UI/ApprovalChangeUI.cs
@@ -8,14 +8,22 @@ public class ApprovalChangeUI : MonoBehaviour
     public Image barLeft;
     public Image barRight;
     public GameObject player;
+    private Animator animator;
 
+  
+    
     public float approval;
     private float previousApproval;
 
     private void Start()
     {
+
+
         approval = player.GetComponent<PlayerScript>().approval.percentage;
         previousApproval = approval;
+
+        animator = GetComponent<Animator>();
+
         barLeft.fillAmount = previousApproval / 100;
         barRight.fillAmount = previousApproval / 100;
     }
@@ -24,6 +32,10 @@ public class ApprovalChangeUI : MonoBehaviour
         approval = player.GetComponent<PlayerScript>().approval.percentage;
         if (approval != previousApproval)
         {
+            if(approval<previousApproval)
+            {
+                animator.SetBool("Hit", true);
+            }
 
             previousApproval = approval;
 
@@ -32,6 +44,7 @@ public class ApprovalChangeUI : MonoBehaviour
         }
         
     }
+
 
 
 }
