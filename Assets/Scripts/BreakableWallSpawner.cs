a8eafcac57dcd7aac9251876f92b8b6345404f77
Kirsty Adair <kirstyaadair@gmail.com>
2020-02-23 16:12:18 +0000

    tiny main scene change

---
+++ Assets/Scripts/BreakableWallSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class WallPattern
+{
+    GameObject[] walls;
+    float timeActive;
+}
+
+public class BreakableWallSpawner : MonoBehaviour
+{
+    
+}
