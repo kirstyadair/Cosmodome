ab007be984e9b54122cddfe4282580010e719c13
Kirsty Adair <kirstyaadair@gmail.com>
2020-03-03 16:43:01 +0000

    spike trap spawning

---
+++ Assets/Scripts/SpikeManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeManager : MonoBehaviour
+{
+    [Header("How long between trap spawns?")]
+    public float timeBetweenSpawns;
+    public float timeSinceLastSpawn;
+    [Header("How long is the trap active?")]
+    public float maxTimeActive;
+    public float timeActive;
+    bool trapsActive = false;
+    [Header("How many traps (max) spawn at once?")]
+    public float numberOfTraps;
+
+    List<SpikeTrapScript> spikeWalls = new List<SpikeTrapScript>();
+    List<SpikeTrapScript> activeTraps = new List<SpikeTrapScript>();
+
+    void Start()
+    {
+        GameObject[] allSpikeWalls = GameObject.FindGameObjectsWithTag("SpikeWall");
+        foreach (GameObject wall in allSpikeWalls)
+        {
+            spikeWalls.Add(wall.GetComponent<SpikeTrapScript>());
+        }
+    }
+
+    void Update()
+    {
+        if (!trapsActive) timeSinceLastSpawn += Time.deltaTime;
+        else timeActive += Time.deltaTime;
+
+        if (timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            SpawnInTraps();
+            timeSinceLastSpawn = 0;
+        } 
+        if (timeActive >= maxTimeActive) 
+        {
+            DisableTraps();
+            timeActive = 0;
+        }
+    }
+
+    void SpawnInTraps()
+    {
+        trapsActive = true;
+
+        for (int i = 0; i < numberOfTraps; i++)
+        {
+            int randomNumber = Random.Range(0, spikeWalls.Count);
+            spikeWalls[randomNumber].SpawnInWall();
+            activeTraps.Add(spikeWalls[randomNumber]);
+        }
+    }
+
+    void DisableTraps()
+    {
+        foreach (SpikeTrapScript trap in activeTraps)
+        {
+            trap.DisableTrap();
+        }
+
+        trapsActive = false;
+    }
+}
--- Assets/Scripts/SpikeTrapScript.cs
+++ Assets/Scripts/SpikeTrapScript.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class SpikeTrapScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // The spikes on this wall
+    List<GameObject> spikes = new List<GameObject>();
+    List<SkinnedMeshRenderer> spikeMRs = new List<SkinnedMeshRenderer>();
+    [SerializeField]
+    GameObject particleEffect;
+    [SerializeField]
+    Material redMat;
+    [SerializeField]
+    Material standardMat;
+
     void Start()
     {
-        
+        GameObject[] allSpikes = GameObject.FindGameObjectsWithTag("Spike");
+        foreach (GameObject spike in allSpikes)
+        {
+            if (spike.transform.parent.transform.parent.name == this.gameObject.name)
+            {
+                spikes.Add(spike);
+                spikeMRs.Add(spike.GetComponent<SkinnedMeshRenderer>());
+                spike.SetActive(false);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SpawnInWall()
     {
-        
+        for (int i = 0; i < spikes.Count; i++)
+        {
+            spikes[i].SetActive(true);
+            spikeMRs[i].material = redMat;
+            GameObject ps = Instantiate(particleEffect, spikes[i].transform.position, spikes[i].transform.rotation);
+        }
+    }
+
+    public void DisableTrap()
+    {
+        for (int i = 0; i < spikes.Count; i++)
+        {
+            GameObject ps = Instantiate(particleEffect, spikes[i].transform.position, spikes[i].transform.rotation);
+            spikeMRs[i].material = standardMat;
+            spikes[i].SetActive(false);
+        }
     }
 }
