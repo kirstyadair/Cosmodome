83e10372c22d09386e35b1f67edfb09593574545
jokehboy <jokehboy@gmail.com>
2020-03-09 17:20:06 +0000

    Fixed percentage bars and that

--- Assets/Scripts/PercentageBar.cs
+++ Assets/Scripts/PercentageBar.cs
@@ -5,40 +5,63 @@ using UnityEngine.UI;
 
 public class PercentageBar : MonoBehaviour
 {
-    public LayoutElement RedPercent;
-    public LayoutElement BluePercent;
-    public LayoutElement OrangePercent;
-    public LayoutElement PurplePercent;
+    
 
-    public GameObject RedPlayer;
-    public GameObject BluePlayer;
-    public GameObject OrangePlayer;
-    public GameObject PurplePlayer;
+    public List<GameObject> percentageColours = new List<GameObject>();
+    public List<GameObject> playerArray = new List<GameObject>();
 
+
+
+    public void OnStateChange(GameState newState, GameState oldState)
+    {
+        if (newState == GameState.ROUND_END_CUTSCENE)
+        {
+            CheckPlayers();
+        }
+        if (newState == GameState.ROUND_START_CUTSCENE)
+        {
+            CheckPlayers();
+        }
+        if (newState == GameState.COUNTDOWN)
+        {
+            CheckPlayers();
+
+        }
+
+
+
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        RedPercent.flexibleWidth = RedPlayer.GetComponent<ApprovalChangeUI>().approval / 100;
-        BluePercent.flexibleWidth = BluePlayer.GetComponent<ApprovalChangeUI>().approval / 100;
-        OrangePercent.flexibleWidth = OrangePlayer.GetComponent<ApprovalChangeUI>().approval / 100;
-        PurplePercent.flexibleWidth = PurplePlayer.GetComponent<ApprovalChangeUI>().approval / 100;
+        ScoreManager.OnStateChanged += OnStateChange;
 
+    }
 
+    void CheckPlayers()
+    {
+        for (int i = 0; i < playerArray.Count; i++)
+        {
+            if (!playerArray[i].activeSelf)
+            {
+                percentageColours[i].SetActive(false);
+            }
+        }
     }
 
     public void UpdatePercentages()
     {
-        RedPercent.flexibleWidth = RedPlayer.GetComponent<ApprovalChangeUI>().approval / 100;
-        BluePercent.flexibleWidth = BluePlayer.GetComponent<ApprovalChangeUI>().approval / 100;
-        OrangePercent.flexibleWidth = OrangePlayer.GetComponent<ApprovalChangeUI>().approval / 100;
-        PurplePercent.flexibleWidth = PurplePlayer.GetComponent<ApprovalChangeUI>().approval / 100;
+        for(int i =0; i<percentageColours.Count;i++)
+        {
+            percentageColours[i].GetComponent<LayoutElement>().flexibleWidth = playerArray[i].GetComponent<ApprovalChangeUI>().approval / 100;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdatePercentages();
     }
 }
