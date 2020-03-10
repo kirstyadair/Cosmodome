a28e30eb47f9ebf12f157b4589e5029009e7cf78
Josh Lee <josh@joshbe.me>
2020-03-10 03:43:05 +0000

    improve percentage bars

--- Assets/Scripts/PercentageBar.cs
+++ Assets/Scripts/PercentageBar.cs
@@ -10,7 +10,9 @@ public class PercentageBar : MonoBehaviour
     public List<GameObject> percentageColours = new List<GameObject>();
     public List<GameObject> playerArray = new List<GameObject>();
 
+    public ApprovalChangeUI thisPlayer;
 
+    public Text statusText;
 
     public void OnStateChange(GameState newState, GameState oldState)
     {
@@ -32,6 +34,22 @@ public class PercentageBar : MonoBehaviour
 
     }
 
+    string GetStatusText(float percentage, int numberOfPlayers)
+    {
+        // for example, 0.25 or above means they are the top player in a 4 player game
+        float best = (float)1 / numberOfPlayers;
+
+        // 0.5 is halfway to the best player for example
+        float percentageOfBest = percentage / best;
+
+        string result = "LOSER";
+
+        if (percentageOfBest >= 0.50f) result = "UP N COMING";
+        if (percentageOfBest >= 0.70f) result = "NECK N NECK";
+        if (percentageOfBest >= 1f) result = "FAVOURITE";
+
+        return result;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -57,6 +75,8 @@ public class PercentageBar : MonoBehaviour
         {
             percentageColours[i].GetComponent<LayoutElement>().flexibleWidth = playerArray[i].GetComponent<ApprovalChangeUI>().approval / 100;
         }
+
+        statusText.text = GetStatusText(thisPlayer.approval / 100, percentageColours.Count);
     }
 
     // Update is called once per frame
