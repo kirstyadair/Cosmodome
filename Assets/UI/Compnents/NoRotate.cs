c17a4941185dd069d4016029892bc3c86171b58c
jokehboy <jokehboy@gmail.com>
2020-03-13 14:01:38 +0000

    Added lil loady icon

---
+++ Assets/UI/Compnents/NoRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoRotate : MonoBehaviour
+{
+    Quaternion rotation;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.rotation = rotation;
+    }
+}
+    
