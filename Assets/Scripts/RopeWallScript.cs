eb470675c8ae49c2ea43b79abf283c47083c6baa
Kirsty Adair <kirstyaadair@gmail.com>
2020-03-22 14:18:24 +0000

    PLEASE WORK

--- Assets/Scripts/RopeWallScript.cs
+++ Assets/Scripts/RopeWallScript.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +8,8 @@ public class RopeWallScript : MonoBehaviour
     Vector3 desiredVelocity;
     Vector3 targetPoint;
     Rigidbody rb;
-    float magnitudeOfShip;
-    Vector3 directionOfShip;
-    Rigidbody shipRB;
-    bool shipHit = false;
-    // This stores the mass prior to the change being made so it can be reset
-    public float prevShipMass;
+    [SerializeField]
+    List<ShipToBounce> ships = new List<ShipToBounce>();
 
     void Start()
     {
@@ -26,15 +23,16 @@ public class RopeWallScript : MonoBehaviour
     {
         // Return to centre point if not already there
         if (Vector3.Distance(transform.localPosition, targetPoint) > 0.2f) SeekCentrePoint();
-        else if (shipHit) PushShip();
+        else if (ships.Count > 0) PushShips();
     }
 
     void SeekCentrePoint()
     {
-        if (shipHit)
+        foreach (ShipToBounce ship in ships)
         {
-            shipRB.mass = 0.5f;
+            ship.shipRB.mass = 0.5f;
         }
+
         desiredVelocity = Vector3.Normalize(targetPoint - transform.localPosition) * 0.3f;
         transform.localPosition += desiredVelocity;
     }
@@ -42,25 +40,46 @@ public class RopeWallScript : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ship"))
-        {   
-            shipRB = collision.gameObject.GetComponent<Rigidbody>();
-            if (shipRB.velocity.magnitude > 10)
-            {
-                if (!shipHit) prevShipMass = shipRB.mass;
-                shipHit = true;
-            }
+        {
+            ShipToBounce newShip = new ShipToBounce();
+            newShip.shipRB = collision.gameObject.GetComponent<Rigidbody>();
+
+            if (newShip.originalMass == 0) newShip.originalMass = newShip.shipRB.mass;
+            newShip.timeUntilRevertMass = 1.0f;
+
+            ships.Add(newShip);
         }
     }
 
-    void PushShip()
+    void PushShips()
     {
-        shipHit = false;
-        shipRB.mass = prevShipMass;
-        ShipController sc = shipRB.gameObject.GetComponent<ShipController>();
-        magnitudeOfShip = shipRB.velocity.magnitude;
-        directionOfShip = Vector3.Normalize(shipRB.gameObject.transform.position - transform.position);
-        directionOfShip.y = 0.5f;
+        foreach (ShipToBounce ship in ships)
+        {
+            ship.shipRB.mass = ship.originalMass;
+            ShipController sc = ship.shipRB.gameObject.GetComponent<ShipController>();
+            ship.directionOfShip = Vector3.Normalize(ship.shipRB.gameObject.transform.position - transform.position);
+            ship.directionOfShip.y = 0.5f;
 
-        sc.PushBack(directionOfShip * 60);
+            sc.PushBack(ship.directionOfShip * 60);
+        }
+    }
+}
+
+[Serializable]
+public class ShipToBounce : MonoBehaviour
+{
+    public Rigidbody shipRB;
+    public float originalMass = 0;
+    public float timeUntilRevertMass;
+    public int playerNumber;
+    public Vector3 directionOfShip;
+
+    private void Update()
+    {
+        if (timeUntilRevertMass > 0)
+        {
+            timeUntilRevertMass -= Time.deltaTime;
+        }
+        else if (timeUntilRevertMass <= 0 && originalMass != 0) shipRB.mass = originalMass;
     }
 }
