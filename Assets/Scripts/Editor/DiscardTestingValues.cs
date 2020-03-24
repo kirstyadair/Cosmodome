3645fa256b0778ef78234ca7c71376f1f8224408
Josh Lee <josh@joshbe.me>
2020-03-24 03:25:24 +0000

    discardtestingvalues

---
+++ Assets/Scripts/Editor/DiscardTestingValues.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Editor script that restores testing values to default when the project is open, to provent test values getting pushed
+/// </summary>
+[ExecuteInEditMode]
+public class DiscardTestingValues: MonoBehaviour
+{
+    public bool _needsRestore = true;
+
+    void Start() {
+        //aa
+        if (!_needsRestore) return;
+        RestoreValues();
+
+        _needsRestore = false;
+    }
+
+    void RestoreValues() {
+        Debug.Log("<b>DiscardTestingValues has set these values to defaults, as you probably didn't mean to push them:</b><br/>Round length");
+    }
+}
---
+++ Assets/Scripts/Util/DiscardTestingValues.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Editor script that restores testing values to default when the project is open, to provent test values getting pushed
+/// </summary>
+[ExecuteInEditMode]
+public class DiscardTestingValues: MonoBehaviour
+{
+    bool _needsRestore = true;
+
+    void Awake() {
+        if (!_needsRestore) return;
+        RestoreValues();
+
+        EditorSceneManager.sceneClosing  += OnSceneClosing;
+        _needsRestore = false;
+    }
+
+    void OnSceneClosing(Scene scene, bool removingScene) {
+        _needsRestore = true;
+    }
+
+    void RestoreValues() {
+        List<string> testingValuesChanged = new List<string>();
+
+        void CheckIfValueChanged(string result) {
+            if (result == null) return;
+            testingValuesChanged.Add(result);
+        }
+
+        ScoreManager sm = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+
+        if (sm != null) {
+            CheckIfValueChanged(RestoreRoundTime(sm));
+        }
+
+        CutscenesManager cm = GameObject.Find("CutscenesManager").GetComponent<CutscenesManager>();
+        if (cm != null) {
+            CheckIfValueChanged(RestoreIntroCutscenes(cm));
+        }
+
+        if (testingValuesChanged.Count == 0) return; // We didn't need to change anything
+
+        string log = "<size=14><color=lightblue><b>DiscardTestingValues has set these values to defaults because test changes were pushed: </b></color>";
+
+        foreach (string valueChanged in testingValuesChanged) {
+            log += "<i> || " + valueChanged + "</i>";
+        }
+
+        log += "</size>";
+        Debug.Log(log);
+    }
+
+
+
+
+
+    /// VALUE RESTORING METHODS
+    /// return string is the friendly name to show in editor, or null if not changed
+
+    string RestoreRoundTime(ScoreManager sm) {
+        bool changed = false;
+
+        if (sm.timeLeftInRound != 60) {
+            sm.timeLeftInRound = 60;
+            changed = true;
+        }
+
+        if (sm.roundLength != 60) {
+            sm.roundLength = 60;
+            changed = true;
+        }
+
+        //if (!changed) return null;
+
+        return "<b>Round length</b> changed to <b>60</b>";
+    }
+
+    string RestoreIntroCutscenes(CutscenesManager cm) {
+        if (!cm.shouldShowIntroCutscenes) {
+            cm.shouldShowIntroCutscenes = true;
+            return "Set <b>ShouldShowIntroCutScenes</b> to <b>true</b>";
+        }
+        
+         return "Set <b>ShouldShowIntroCutScenes</b> to <b>true</b>";
+        return null;
+    }
+
+
+}
