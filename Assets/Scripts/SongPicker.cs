0647118d8ae4d73e2aaa8347e636590fc19a4b80
jokehboy <jokehboy@gmail.com>
2020-04-07 15:12:33 +0100

    Seamless songs
    happi boi

--- Assets/Scripts/SongPicker.cs
+++ Assets/Scripts/SongPicker.cs
@@ -14,26 +14,16 @@ public class Song
 public class SongPicker : MonoBehaviour
 {
     public List<Song> theSongs = new List<Song>();
-
-    public List<AudioClip> songs;
    
     public AudioSource currentSong;
     public AudioSource currentSongFill;
 
+    
+
     public int roundCounter = 1;
     int songChosen;
 
-    public float timepassed;
-    public float secPerBeat;
-    public float songPosition;
-    public float songPositionInBeats;
-
-    public bool countBeats = true;
-
-    public float timeToStartFrom;
-
-    public float firstBeatOffset;
-    public float currentSongTime;
+    
 
 
     public void OnStateChange(GameState newState, GameState oldState)
@@ -44,32 +34,18 @@ public class SongPicker : MonoBehaviour
         }
         if (newState == GameState.ROUND_END_CUTSCENE)
         {
-            timeToStartFrom = songPosition;
-            currentSong.Stop();
-            countBeats = false;
-            currentSongFill.Play();
             roundCounter++;
         }
         if(newState==GameState.ROUND_START_CUTSCENE)
         {
-            countBeats = false;
+            
         }
         if (newState == GameState.INGAME)
         {
-            currentSongFill.Stop();
-            if(roundCounter>2)
+            if(roundCounter==1)
             {
-                currentSongFill.clip = null;
+                StartCoroutine(PlaySong());
             }
-            else
-            {
-                currentSongFill.clip = theSongs[songChosen].songFills[roundCounter - 1];
-            }
-            
-
-            countBeats = true;
-            currentSong.time = timeToStartFrom;
-            currentSong.Play();
         }
 
     }
@@ -87,29 +63,49 @@ public class SongPicker : MonoBehaviour
 
         int songtoPick = Random.Range(0, theSongs.Count);
         songChosen = songtoPick;
-        
-        secPerBeat = 60f/theSongs[songtoPick].songBPM;
 
         currentSong.clip = theSongs[songtoPick].song;
         currentSongFill.clip = theSongs[songtoPick].songFills[0];
 
-        currentSongTime = (float)AudioSettings.dspTime;
+        
+
+    }
 
 
+    void CheckCurrentRound()
+    {
+        if (roundCounter > 2)
+        {
+            currentSongFill.clip = null;
+        }
+        else
+        {
+            currentSongFill.clip = theSongs[songChosen].songFills[roundCounter - 1];
+        }
+    }
+
+
+    IEnumerator PlaySong()
+    {
+        CheckCurrentRound();
+        currentSong.Play();
+        yield return new WaitForSeconds(60.0f);
+        currentSong.Pause();
+        StartCoroutine(PlaySongFill());
+    }
+
+    IEnumerator PlaySongFill()
+    {
+        currentSongFill.Play();
+        yield return new WaitForSeconds(15.0f);
+        currentSongFill.Stop();
+        StartCoroutine(PlaySong());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentSong.isPlaying && countBeats == true)
-        {
-            songPosition = (float)(AudioSettings.dspTime - currentSongTime - firstBeatOffset - timepassed);
-            songPositionInBeats = songPosition / secPerBeat;
-        }
-        
-        if(countBeats == false)
-        {
-            timepassed += Time.deltaTime;
-        }
+
+
     }
 }
